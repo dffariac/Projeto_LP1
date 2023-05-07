2b6fa004cb3ca832e9be5ba5ec0c03a531c63233
dffariac <faria.fernando@aluno.ifsp.edu.br>
2023-05-07 15:26:22 -0300

    Atualização da classe de funcionários com métodos das interfaces.

--- LP1A3_Projeto1/src/main/java/org/grupo2/modelos/Funcionario.java
+++ LP1A3_Projeto1/src/main/java/org/grupo2/modelos/Funcionario.java
@@ -1,6 +1,7 @@
 package org.grupo2.modelos;
 
 import java.util.Map;
+import java.util.Scanner;
 
 import org.grupo2.interfaces.GerenciamentoDeLivros;
 import org.grupo2.interfaces.GerenciamentoDeUsuarios;
@@ -65,9 +66,43 @@ public class Funcionario extends Usuario implements GerenciamentoDeUsuarios, Ger
 
     @Override
     public void atualizarLivro(Livro livro){
-        Biblioteca.getLivros().put(48, livro);
-        //Adicionar logica para verificar livro por titulo e adicionar ao numero de exemplares
-        return;
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < bdLivro.size(); i++) {
+            if (livro.equals(bdLivro.getClass())) {
+                System.out.println("O que deseja atualizar? \n" +
+                        "1- Titulo \n " +
+                        "2 - Autor \n " +
+                        "3 - Quantidade disponivel \n" +
+                        "4- Id do livro");
+                int opcao = scan.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Digite o novo nome do livro:");
+                        String mudaNome = scan.next();
+                        livro.setTitulo(mudaNome);
+                        break;
+                    case 2:
+                        System.out.println("Digite o novo nome do Autor:");
+                        String mudaAutor = scan.next();
+                        livro.setAutor(mudaAutor);
+                        break;
+                    case 3:
+                        System.out.println("Digite a nova quantidade disponivel:");
+                        int mudaQuantidade = scan.nextInt();
+                        livro.setNumExemplaresDisponiveis(mudaQuantidade);
+                        break;
+                    case 4:
+                        System.out.println("Digite o novo ID:");
+                        int mudaID = scan.nextInt();
+                        livro.setId(mudaID);
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+                scan.close();
+
+            }
+        }
     }
 
     @Override
@@ -87,22 +122,88 @@ public class Funcionario extends Usuario implements GerenciamentoDeUsuarios, Ger
 
     @Override
     public void cadastrarUsuario( Usuario usuario ){
-        
+        for (int i = 0; i < bdUsuario.size(); i++) {
+            if (usuario.equals(bdUsuario.get(i))) {
+                System.out.println("Usuário já existe no sistema");
+                break;
+            } else {
+                bdUsuario.put(null, usuario);
+                break;
+            }
+        }
     }
 
     @Override
     public void atualizarUsuario(Usuario usuario){
-        return;
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < bdLivro.size(); i++) {
+            if (usuario.equals(bdUsuario.getClass())) {
+                System.out.println("O que deseja atualizar? \n" +
+                        "1- Nome \n " +
+                        "2 - Email \n " +
+                        "3 - Endereço \n" +
+                        "4- Id do usuario");
+                int opcao = scan.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Digite o novo nome do usuário:");
+                        String mudaNome = scan.next();
+                        usuario.setNome(mudaNome);
+                        break;
+                    case 2:
+                        System.out.println("Digite o novo nome email:");
+                        String mudaEmail = scan.next();
+                        usuario.setEmail(mudaEmail);
+                        break;
+                    case 3:
+                        System.out.println("Digite o novo endereço do Usuário:");
+                        String mudaEndereco = scan.next();
+                        usuario.setEndereco(mudaEndereco);
+                        break;
+                    case 4:
+                        System.out.println("Digite a nova senha:");
+                        String mudaSenha = scan.next();
+                        usuario.setSenha(mudaSenha);
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+                scan.close();
+
+            }
+        }
     }
 
     @Override
     public void removerUsuario(Usuario usuario){
-        return;
+        try {
+            for (int i = 0; i < bdUsuario.size(); i++) {
+                if (usuario.getNome().equals(bdUsuario.get(i).getNome())) {
+                    System.out.println("Usuario " + bdUsuario.get(i).getNome() + "removido");
+                    bdUsuario.remove(null, usuario);
+                    break;
+                }
+            }
+        }
+        catch (Exception erro5) {
+            System.out.println("Usuário não encontrado");
+        }
     }
 
     @Override
     public void buscarUsuario(Usuario usuario){
-        return;
+        try {
+            for (int i = 0; i < bdUsuario.size(); i++) {
+                if (usuario.equals(bdUsuario.get(i))) {
+                    System.out.println("Usuário encontrado!" + "\n Nome:" + bdUsuario.get(i).getNome() +
+                            "\n Email :" + bdUsuario.get(i).getEmail());
+                }
+            }
+        } catch (Exception error) {
+            System.out.println("Usuaŕio não encontrado");
+        }
+
+    }
     }
     
     public String toJson() {
